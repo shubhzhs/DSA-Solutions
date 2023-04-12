537e38daecfea90b8ac2bd9cc5981339fae68651
Shubham Sonawane <82930194+shubhzhs@users.noreply.github.com>
2023-04-12 18:37:00 +0530

    Add files via upload

---
+++ Find the Index of the First Occurrence in a String/Javascript Solution.js
@@ -0,0 +1,9 @@
+var strStr = function(haystack, needle) {
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        if (haystack.slice(i, i + needle.length) === needle) {
+            return i;
+        }
+    }
+    
+    return -1;
+};
